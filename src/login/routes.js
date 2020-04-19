1c5b84853dce09cef8c6dc4184e851144d283eaf
Wagner Graciano Junior <wagner_graciano0101@hotmail.com>
2020-04-19 15:05:30 -0300

    creating auth files and structure

---
+++ src/login/routes.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import { isAuthenticated } from "./auth";
+
+import { Route, Switch, Redirect } from "react-router-dom";
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      isAuthenticated() ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to={{ pathname: "/", state: { from: props.location } }} />
+      )
+    }
+  />
+);
+
+const Routes = () => (
+//   <Switch>
+//     <Route exact={true} path="/" component={Login} />
+//     <PrivateRoute path="/login" component={Login} />
+//     <PrivateRoute path="/config" component={Config} />
+//     <PrivateRoute path="/comm" component={Comm} />
+//     <PrivateRoute path="/reports" component={Reports} />
+//   </Switch>
+);
+
+export default Routes;
